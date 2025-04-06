afb581e4c8b1f1c5d70550f345b9d7740b1471ac
SaquibNazeer01 <bhatsaakib505@gmail.com>
2025-04-06 21:12:38 +0530

    Update manualProjects.js

--- js/manualProjects.js
+++ js/manualProjects.js
@@ -62,7 +62,7 @@ function loadManualProjects() {
             },
             {
                 name: "Face Recognition Attendance system",
-                description: "Developing an attendance system which will use students face to register the attendance of the respected students.",
+                description: "Developed an attendance system which will use students face to register the attendance of the respected students.",
                 status: "Completed",
                 githubUrl: null,
                 deployUrl: "https://youtu.be/zxWtZIFV2-U?si=a2Fitm5zYI5f07B5",
